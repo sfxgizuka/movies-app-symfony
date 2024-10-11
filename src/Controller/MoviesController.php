14db4d7bdf3d23e3174b07b2c3de823631d28142
Gregory Izuka <gizuka@seamfix.com>
2024-10-11 20:45:08 +0100

    modifications

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -12,8 +12,7 @@ class MoviesController extends AbstractController
     #[Route('/movies', name: 'app_movies', defaults: ['name' => null], methods: ['GET', 'HEAD'])]
     public function index(): Response
     {
-        return $this->render('index.html.twig', [
-            'title' => 'Marvel Movies'
-        ]);
+        $movies = ['Aki & Paw Paw', 'The Shawshank Redemption', 'The Godfather', 'The Godfather: Part II', 'The Dark Knight', '12 Angry Men', 'Schindler\'s List', 'The Lord of the Rings: The Return of the King', 'Pulp Fiction', 'The Good, the Bad and the Ugly', 'The Lord of the Rings: The Fellowship of the Ring'];
+        return $this->render('index.html.twig', array('movies' => $movies));
     }
 }
