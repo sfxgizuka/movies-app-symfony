400f9266db8bd5f30c6c84514fffb3184ab197d7
Gregory Izuka <gizuka@seamfix.com>
2024-10-16 04:16:46 +0100

    Added some views and tailwind config

---
+++ assets/javascript/method2.js
@@ -0,0 +1,1 @@
+alert("Method 2");
--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -21,20 +21,27 @@ class MoviesController extends AbstractController
 //     }
 // }
 {
-    private $em;
-    public function __construct(EntityManagerInterface $em){
-        $this->em = $em;
+    private $movieRepository;
+    public function __construct(MovieRepository $movieRepository){
+        $this->movieRepository = $movieRepository;
     }
-    #[Route('/movies', name: 'app_movies', defaults: ['name' => null], methods: ['GET', 'HEAD'])]
+    #[Route('/movies', methods: ['GET'], name: 'app_movies')]
     public function index(): Response
     {
         //findAll ==> select * from movies
         //find ==> select * from movies where id = 5
         //findBy ==> select * from movies order by id desc;
         //findOneBy ==> select * from movies where id = 5
-        $movieRepository = $this->em->getRepository(Movie::class);
-        $movies = $movieRepository->findOneBy(['id'=>5, 'title'=>'aki and paw paw'], ['id' => 'DESC']);
-        dd($movies);
-        return $this->render('index.html.twig');
+        return $this->render('movies/index.html.twig', [
+            'movies' => $this->movieRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/movies/{id}', methods: ['GET'], name: 'show_movies')]
+    public function show($id): Response
+    {
+        return $this->render('movies/show.html.twig', [
+            'movie' => $this->movieRepository->find($id),
+        ]);
     }
 }
