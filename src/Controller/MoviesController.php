bc8cee579412cfedfb60b83715a14cf00f84a9db
Gregory Izuka <gizuka@seamfix.com>
2024-10-16 05:35:14 +0100

    Added post

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,12 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Form\MovieFormType;
 use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class MoviesController extends AbstractController
 // {
@@ -22,8 +26,10 @@ class MoviesController extends AbstractController
 // }
 {
     private $movieRepository;
-    public function __construct(MovieRepository $movieRepository){
+    private $em;
+    public function __construct(MovieRepository $movieRepository, EntityManagerInterface $em){
         $this->movieRepository = $movieRepository;
+        $this->em = $em;
     }
     #[Route('/movies', methods: ['GET'], name: 'app_movies')]
     public function index(): Response
@@ -34,6 +40,38 @@ class MoviesController extends AbstractController
         //findOneBy ==> select * from movies where id = 5
         return $this->render('movies/index.html.twig', [
             'movies' => $this->movieRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/movies/create', name: 'create_movies')]
+    public function create(Request $request): Response{
+        $movie = new Movie();
+        $form = $this->createForm(MovieFormType::class, $movie);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $newMovie = $form->getData();
+            $imagePath = $form->get('imagePath')->getData();
+            if($imagePath){
+                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+                try{
+                    $imagePath->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                }catch(FileException $e){
+                    return new Response($e->getMessage());
+                }
+                $newMovie->setImagePath('/uploads/' . $newFileName);
+            }
+            $this->em->persist($newMovie);
+            $this->em->flush();
+            return $this->redirectToRoute('app_movies');
+
+        }
+
+        return $this->render('/movies/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
